6c8c288f57d7499db66968fe37ad26659ad58b9b
EunHyeok <secun77@naver.com>
2025-04-14 10:26:49 +0900

    파일명 정리

--- src/routes/router-config.jsx
+++ src/routes/router-config.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from '../components/RootLayout';
+import AboutSpc from '../pages/AboutSpc';
 
 export const routes = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <AboutSpc />,
+      },
+    ],
   },
 ]);
