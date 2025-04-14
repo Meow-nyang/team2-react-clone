9b5bc58b3ff45c6f16eb5b0ad86f5255e481e298
Meow_nyang <slah76@naver.com>
2025-04-14 17:47:16 +0900

    SupportPage

--- src/pages/SupportPage.jsx
+++ src/pages/SupportPage.jsx
@@ -1,37 +1,44 @@
 import React from 'react';
 import styles from '../styles/SupportPage.module.scss';
-import Image from 'next/image';
+
+// 이미지 import (src/assets/images 기준)
+import parisBaguette from '../assets/images/parisBaqeutte.webp';
+import baskinRobbins from '../assets/images/baskinrabins.webp';
+import dunkin from '../assets/images/dunkin.webp';
+import pascucci from '../assets/images/pasqucci.webp';
+import biteun from '../assets/images/biteun.webp';
+import jambaJuice from '../assets/images/kambajuice.webp';
 
 const brands = [
   {
     title: '파리바게뜨',
     description: '베이커리 업계의 부동의 1위',
-    image: '/images/paris_baguette.png',
+    image: parisBaguette,
   },
   {
     title: '배스킨라빈스',
     description: '행복을 전하는 프리미엄 아이스크림',
-    image: '/images/baskinrobbins.png',
+    image: baskinRobbins,
   },
   {
     title: '던킨',
     description: '세계 최대의 커피&도넛 전문 브랜드',
-    image: '/images/dunkin.png',
+    image: dunkin,
   },
   {
     title: '파스쿠찌',
     description: '완벽한 커피, 감각적인 공간',
-    image: '/images/pascucci.png',
+    image: pascucci,
   },
   {
     title: '빚은',
     description: '맛에 건강을 담은 프리미엄 떡',
-    image: '/images/bizeun.png',
+    image: biteun,
   },
   {
     title: '잠바주스',
     description: '리얼 과일 스무디 전문 브랜드',
-    image: '/images/jambajuice.png',
+    image: jambaJuice,
   },
 ];
 
@@ -49,13 +56,7 @@ const SupportPage = () => {
       <div className={styles.grid}>
         {brands.map((brand, index) => (
           <div className={styles.card} key={index}>
-            <Image
-              src={brand.image}
-              alt={brand.title}
-              width={300}
-              height={200}
-              className={styles.image}
-            />
+            <img src={brand.image} alt={brand.title} className={styles.image} />
             <div className={styles.cardText}>
               <h3>{brand.title}</h3>
               <p>{brand.description}</p>
