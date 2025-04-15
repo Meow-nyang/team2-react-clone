2f373639b5d9522f547f4c467a798af982a600f1
Meow_nyang <slah76@naver.com>
2025-04-15 11:30:59 +0900

    MainPage 완료

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,41 +5,44 @@ import styles from '../styles/MainPage.module.scss';
 import parisBaguette from '../assets/images/parisBaqeutte.webp';
 import baskinRobbins from '../assets/images/baskinrabins.webp';
 import dunkin from '../assets/images/dunkin.webp';
-import pascucci from '../assets/images/pasqucci.webp';
+// import pascucci from '../assets/images/pasqucci.webp';
 
 const brands = [
   {
     title: '파리바게뜨',
-    description: '베이커리 업계의 부동의 1위',
     image: parisBaguette,
   },
   {
     title: '배스킨라빈스',
-    description: '행복을 전하는 프리미엄 아이스크림',
     image: baskinRobbins,
   },
   {
     title: '던킨',
-    description: '세계 최대의 커피&도넛 전문 브랜드',
     image: dunkin,
   },
-  {
-    title: '파스쿠찌',
-    description: '완벽한 커피, 감각적인 공간',
-    image: pascucci,
-  },
+  // {
+  //   title: '파스쿠찌',
+  //   image: pascucci,
+  // },
 ];
 
 const MainPage = () => {
   return (
     <div className={styles.supportPage}>
-      <div className={styles.grid}>
+      {/* 상단 큰 블록 */}
+      <div className={`${styles.heroBanner} ${styles.slideInUp}`}>
+        <div className={styles.heroContent}>
+          <h2 className={styles.fadeSlideUp}>일상을 맛있게 행복하게</h2>
+          <p className={styles.fadeSlideDown}>Happiness in every bite</p>
+        </div>
+      </div>
+
+      <div className={`${styles.grid} ${styles.slideIn}`}>
         {brands.map((brand, index) => (
           <div className={styles.card} key={index}>
             <img src={brand.image} alt={brand.title} className={styles.image} />
             <div className={styles.cardText}>
               <h3>{brand.title}</h3>
-              <p>{brand.description}</p>
             </div>
           </div>
         ))}
