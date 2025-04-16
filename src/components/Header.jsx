05f4715f79d654956724ae558fd7f02b9589739c
EunHyeok <secun77@naver.com>
2025-04-16 12:24:40 +0900

    헤더 및 푸터 반응형 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/Header.module.scss';
 import { Link } from 'react-router-dom';
 import HeaderList from './HeaderList';
 import LanguageSelector from './LanguageSelector';
 import logo from '../assets/images/SPC_logo_core_alt_color-removebg-preview.png';
+import MobileMenu from './MobileMenu';
 
 const Header = () => {
   const [isHoverMenu, setIsHoverMenu] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const handleMouseOn = (flag) => {
     setIsHoverMenu(flag);
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <header className={styles.header}>
@@ -18,49 +29,69 @@ const Header = () => {
         <Link to='/' className={styles.logo}>
           <img src={logo} alt='SPC 로고' />
         </Link>
-        <nav className={styles.nav}>
-          <ul className={styles.right} onMouseEnter={() => handleMouseOn(true)}>
-            <li>
-              <Link to='/about'>ABOUT SPC</Link>
-            </li>
-            <li>
-              <Link to='/'>HISTORY</Link>
-            </li>
-            <li>
-              <Link to='/'>BRAND</Link>
-            </li>
-            <li>
-              <Link to='/'>SPC NOW</Link>
-            </li>
-            <li>
-              {/* /esg -> /esg/management로 수정함  */}
-              <Link to='/esg/management'>ESG</Link>
-            </li>
-            <li>
-              <Link to='/support'>SUPPORT</Link>
-            </li>
-          </ul>
+        {windowWidth > 1024 ? (
+          <nav className={styles.nav}>
+            <ul
+              className={styles.right}
+              onMouseEnter={() => handleMouseOn(true)}
+            >
+              <li>
+                <Link to='/about' className={styles.lii}>
+                  ABOUT SPC
+                </Link>
+              </li>
+              <li>
+                <Link to='/' className={styles.lii}>
+                  HISTORY
+                </Link>
+              </li>
+              <li>
+                <Link to='/' className={styles.lii}>
+                  BRAND
+                </Link>
+              </li>
+              <li>
+                <Link to='/' className={styles.lii}>
+                  SPC NOW
+                </Link>
+              </li>
+              <li>
+                {/* /esg -> /esg/management로 수정함  */}
+                <Link to='/esg/management' className={styles.lii}>
+                  ESG
+                </Link>
+              </li>
+              <li>
+                <Link to='/support' className={styles.lii}>
+                  SUPPORT
+                </Link>
+              </li>
+            </ul>
 
-          <ul className={styles.left}>
-            <li>
-              <button
-                className={styles.career}
-                onClick={() =>
-                  window.open(
-                    'https://spc.recruiter.co.kr/career/home',
-                    '_blank',
-                    'noopener,noreferrer',
-                  )
-                }
-              >
-                CAREERS
-              </button>
-            </li>
-            <li>
-              <LanguageSelector />
-            </li>
-          </ul>
-        </nav>
+            <ul className={styles.left}>
+              <li>
+                <button
+                  className={styles.career}
+                  onClick={() =>
+                    window.open(
+                      'https://spc.recruiter.co.kr/career/home',
+                      '_blank',
+                      'noopener,noreferrer',
+                    )
+                  }
+                >
+                  CAREERS
+                </button>
+              </li>
+              <li>
+                <LanguageSelector />
+              </li>
+            </ul>
+          </nav>
+        ) : (
+          <MobileMenu />
+        )}
+
         {isHoverMenu && (
           <div className={styles.dropdown}>
             <HeaderList onMouse={handleMouseOn} />
